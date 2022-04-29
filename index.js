41b4576f46777418647e464aa42fa36e3f9e4986
abmali05 <bestofhonest@gmail.com>
2022-04-29 15:39:04 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@cluster0.n2zk3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoryCollection = client.db('inventory').collection('product');
+        app.get('/manageitems', async (req, res) => {
+
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+
+        })
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('Inventory server is runnung successfully')
 });
