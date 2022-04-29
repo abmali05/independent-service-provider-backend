8d97eb0f3ee01cb8718a14c6db675c09bb47c9a3
abmali05 <bestofhonest@gmail.com>
2022-04-29 12:22:19 +0600

    server runnung test

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Inventory server is runnung successfully')
+});
+
+app.listen(port, () => {
+    console.log('Inventory server is runnung on  port', port);
+})
